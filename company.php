a3f721f702f11b23db3da0b3727d6d3136023ee0
Deepen Dhamecha <deependhamecha@acadnion.com>
2018-04-19 17:59:19 +0530

    Added Press Page and Client Logos to Company Page

--- company.php
+++ company.php
@@ -33,6 +33,189 @@
         Lorem ipsum dolor sit amet, augue singulis reprimique eu nam, in sea quis omnes, eam et verear diceret. In per quidam luptatum, cibo ludus et has, ex esse omnium eum. Nonumy admodum nam ex, dolor consul vim an.
       </p>
     </div>
+
+    <h1 class="client-heading">Our Clients</h1>
+    <div class="client-bg">
+      <div class="row container client-logos-wrapper">
+        <div class="client-logos">
+          <div class="client-logo-wrapper">
+            <img src="images/client_logos/client1.svg" alt="" class="client-logo" />
+          </div>
+          <div class="client-logo-wrapper">
+            <img src="images/client_logos/client2.svg" alt="" class="client-logo" />
+          </div>
+
+          <div class="client-logo-wrapper">
+            <img src="images/client_logos/client3.svg" alt="" class="client-logo" />
+          </div>
+
+          <div class="client-logo-wrapper">
+            <img src="images/client_logos/client4.svg" alt="" class="client-logo" />
+          </div>
+        </div>
+
+        <div class="client-logos">
+
+          <div class="client-logo-wrapper">
+            <img src="images/client_logos/client5.svg" alt="" class="client-logo" />
+          </div>
+
+          <div class="client-logo-wrapper">
+            <img src="images/client_logos/client6.svg" alt="" class="client-logo" />
+          </div>
+
+          <div class="client-logo-wrapper">
+            <img src="images/client_logos/client7.svg" alt="" class="client-logo" />
+          </div>
+
+          <div class="client-logo-wrapper">
+            <img src="images/client_logos/client8.svg" alt="" class="client-logo" />
+          </div>
+        </div>
+
+        <div class="client-logos">
+
+          <div class="client-logo-wrapper">
+            <img src="images/client_logos/client9.svg" alt="" class="client-logo" />
+          </div>
+
+          <div class="client-logo-wrapper">
+            <img src="images/client_logos/client10.svg" alt="" class="client-logo" />
+          </div>
+
+          <div class="client-logo-wrapper">
+            <img src="images/client_logos/client11.svg" alt="" class="client-logo" />
+          </div>
+
+          <div class="client-logo-wrapper">
+            <img src="images/client_logos/client12.svg" alt="" class="client-logo" />
+          </div>
+        </div>
+
+        <div class="client-logos">
+          <div class="client-logo-wrapper">
+            <img src="images/client_logos/client13.svg" alt="" class="client-logo" />
+          </div>
+
+          <div class="client-logo-wrapper">
+            <img src="images/client_logos/client14.svg" alt="" class="client-logo" />
+          </div>
+
+          <div class="client-logo-wrapper">
+            <img src="images/client_logos/client15.svg" alt="" class="client-logo" />
+          </div>
+
+          <div class="client-logo-wrapper">
+            <img src="images/client_logos/client16.svg" alt="" class="client-logo" />
+          </div>
+        </div>
+
+        <div class="client-logos">
+
+          <div class="client-logo-wrapper">
+            <img src="images/client_logos/client17.svg" alt="" class="client-logo" />
+          </div>
+
+          <div class="client-logo-wrapper">
+            <img src="images/client_logos/client18.svg" alt="" class="client-logo" />
+          </div>
+
+          <div class="client-logo-wrapper">
+            <img src="images/client_logos/client19.svg" alt="" class="client-logo" />
+          </div>
+
+          <div class="client-logo-wrapper">
+            <img src="images/client_logos/client21.svg" alt="" class="client-logo" />
+          </div>
+        </div>
+
+        <div class="client-logos">
+          <div class="client-logo-wrapper">
+            <img src="images/client_logos/client22.svg" alt="" class="client-logo" />
+          </div>
+
+          <div class="client-logo-wrapper">
+            <img src="images/client_logos/client23.svg" alt="" class="client-logo" />
+          </div>
+
+          <div class="client-logo-wrapper">
+            <img src="images/client_logos/client24.svg" alt="" class="client-logo" />
+          </div>
+
+          <div class="client-logo-wrapper">
+            <img src="images/client_logos/client25.svg" alt="" class="client-logo" />
+          </div>
+        </div>
+
+        <div class="client-logos">
+          <div class="client-logo-wrapper">
+            <img src="images/client_logos/client26.svg" alt="" class="client-logo" />
+          </div>
+
+          <div class="client-logo-wrapper">
+            <img src="images/client_logos/client27.svg" alt="" class="client-logo" />
+          </div>
+
+          <div class="client-logo-wrapper">
+            <img src="images/client_logos/client28.svg" alt="" class="client-logo" />
+          </div>
+
+          <div class="client-logo-wrapper">
+            <img src="images/client_logos/client29.svg" alt="" class="client-logo" />
+          </div>
+        </div>
+
+        <div class="client-logos">
+
+          <div class="client-logo-wrapper">
+            <img src="images/client_logos/client30.svg" alt="" class="client-logo" />
+          </div>
+
+          <div class="client-logo-wrapper">
+            <img src="images/client_logos/client31.svg" alt="" class="client-logo" />
+          </div>
+
+          <div class="client-logo-wrapper">
+            <img src="images/client_logos/client32.svg" alt="" class="client-logo" />
+          </div>
+
+          <div class="client-logo-wrapper">
+            <img src="images/client_logos/client33.svg" alt="" class="client-logo" />
+          </div>
+        </div>
+
+        <div class="client-logos">
+
+          <div class="client-logo-wrapper">
+            <img src="images/client_logos/client34.svg" alt="" class="client-logo" />
+          </div>
+
+          <div class="client-logo-wrapper">
+            <img src="images/client_logos/client35.svg" alt="" class="client-logo" />
+          </div>
+
+          <div class="client-logo-wrapper">
+            <img src="images/client_logos/client36.svg" alt="" class="client-logo" />
+          </div>
+
+          <div class="client-logo-wrapper">
+            <img src="images/client_logos/client37.svg" alt="" class="client-logo" />
+          </div>
+        </div>
+
+        <div class="client-logos">
+          <div class="client-logo-wrapper">
+            <img src="images/client_logos/client39.svg" alt="" class="client-logo" />
+          </div>
+
+          <div class="client-logo-wrapper">
+            <img src="images/client_logos/client40.svg" alt="" class="client-logo" />
+          </div>
+
+        </div>
+
+      </div>
+    </div>
   </section>
 
   <!-- Footer -->
---
+++ press.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+  <title>Talent Stack</title>
+  <meta http-equiv="content-language" content="en" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link type="text/css" rel="stylesheet" href="css/reset.min.css" />
+  <link type="text/css" rel="stylesheet" href="css/bootstrap.min.css" />
+  <link type="text/css" rel="stylesheet" href="css/style.css" />
+  <link type="text/css" rel="stylesheet" href="css/responsive.css" />
+</head>
+<body>
+
+  <!-- Header -->
+  <?php
+    require_once(dirname(__FILE__).DIRECTORY_SEPARATOR.'includes'.DIRECTORY_SEPARATOR.'header.php');
+  ?>
+
+  <div class="press-banner-wrapper">
+    <h1 class="press-banner-heading">Press</h1>
+    <img src="images/press/press.png" alt="" class="press-banner-img" />
+    <img src="images/press/press_mobile.png" alt="" class="press-banner-img-mobile" />
+  </div>
+
+  <div class="container">
+    <h1 class="press-desc">
+      Lorem ipsum dolor sit amet, augue singulis reprimique eu nam, in sea quis omnes, eam et verear diceret. In per quidam luptatum, cibo ludus et has, ex esse omnium eum. Nonumy admodum nam ex, dolor consul vim an.
+      Lorem ipsum dolor sit amet, augue singulis reprimique eu nam, in sea quis omnes, eam et verear diceret. In per quidam luptatum, cibo ludus et has, ex esse omnium eum. Nonumy admodum nam ex, dolor consul vim an.
+      Lorem ipsum dolor sit amet, augue singulis reprimique eu nam, in sea quis omnes, eam et verear diceret. In per quidam luptatum, cibo ludus et has, ex esse omnium eum. Nonumy admodum nam ex, dolor consul vim an.
+      Lorem ipsum dolor sit amet, augue singulis reprimique eu nam, in sea quis omnes, eam et verear diceret. In per quidam luptatum, cibo ludus et has, ex esse omnium eum. Nonumy admodum nam ex, dolor consul vim an.
+      Lorem ipsum dolor sit amet, augue singulis reprimique eu nam, in sea quis omnes, eam et verear diceret. In per quidam luptatum, cibo ludus et has, ex esse omnium eum. Nonumy admodum nam ex, dolor consul vim an.
+    </h1>
+  </div>
+  <!-- Footer -->
+  <?php
+    require_once(dirname(__FILE__).DIRECTORY_SEPARATOR.'includes'.DIRECTORY_SEPARATOR.'footer.php');
+  ?>
+
+  <script type="text/javascript" src="js/jquery.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="js/script.js"></script>
+</body>
+</html>
